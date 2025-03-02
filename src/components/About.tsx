6d3f8a90e6c3a67becc824380f8bd6e86242f92d
Dominick Thornton <domthor@umich.edu>
2025-03-01 23:17:49 -0500

    About Component

---
+++ src/components/About.tsx
@@ -0,0 +1,26 @@
+import aboutImg from "../assets/Nature.png";
+import { ABOUT_TEXT } from "../constants";
+
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="mt-20 mb-10 text-center text-4xl lg:text-6xl">About Me</h1>
+      <div className="flex flex-wrap align-top">
+        <div className="w-full lg:w-1/2">
+          <div className="flex items-center justify-center">
+            <img src={aboutImg} alt="About Me" className="rounded-3xl m-9" />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {ABOUT_TEXT}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
